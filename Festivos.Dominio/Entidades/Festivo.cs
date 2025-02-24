da3a85530ffccd8b3f7925e54de672db76b6779e
Juanca_0011 <juan.londono491@pascualbravo.edu.co>
2025-02-24 11:48:10 -0500

    Primer parte proyecto

---
+++ Festivos.Dominio/Entidades/Festivo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Festivos.Dominio.Entidades
+{
+    [Table("Festivo")]
+    public class Festivo
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Nombre")]
+        public required string Nombre { get; set; }
+
+        [Column("Dia")]
+        public int Dia { get; set; }
+
+        [Column("Mes")]
+        public int Mes { get; set; }
+
+        [Column("DiasPascua")]
+        public int DiasPascua { get; set; }
+
+        [Column("TipoId")]
+
+        public int TipoId { get; set; }
+
+        public Tipo TipoDia { get; set; }
+    }
+}
